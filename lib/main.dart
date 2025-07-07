4d7988e295b89bbaccdbd5e465a01cec4ffa11c4
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-07 23:57:52 +0530

    List Widget ArrayList

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,39 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<int> items = [];
+
+  @override
+  void initState() {
+    for (int i = 0; i < 50; i++) {
+      items.add(i);
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'My Application',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('List Widget')),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('List 1'),
+        appBar: AppBar(title: Text('List Widget - ArrayList')),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text('Item no $index'),
               trailing: Icon(Icons.arrow_forward),
-            ),
-            ListTile(
-              title: Text('List 2'),
-              trailing: Icon(Icons.arrow_forward),
-            ),
-            ListTile(
-              title: Text('List 3'),
-              trailing: Icon(Icons.arrow_forward),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
