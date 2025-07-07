bf507d2e3caefc23f6358245194d2ed65d73b3dc
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-08 01:21:37 +0530

    Appbar and TabBar Widgets with their properties

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,13 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  List<int> items = [];
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  String mtext = '';
 
+  late TabController _tcontorl;
   @override
   void initState() {
-    for (int i = 0; i < 50; i++) {
-      items.add(i);
-    }
+    _tcontorl = TabController(length: 3, vsync: this);
     super.initState();
   }
 
@@ -28,18 +27,67 @@ class _MyAppState extends State<MyApp> {
       title: 'My Application',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Gridview Widget')),
-        body: GridView.builder(
-          itemCount: items.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('Appbar'),
+          //centerTitle: true,
+          titleSpacing: 80.0,
+          elevation: 30.0,
+          //toolbarOpacity: 0.5,
+          leading: IconButton(
+            onPressed: () {
+              setState(() {
+                mtext = 'This is menu icon button';
+              });
+            },
+            icon: Icon(Icons.menu),
           ),
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              color: Colors.green,
-              child: Padding(padding: EdgeInsetsGeometry.all(20.0)),
-            );
-          },
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  mtext = 'This is arrow icon button';
+                });
+              },
+              icon: Icon(Icons.arrow_forward),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  mtext = 'This is close icon button';
+                });
+              },
+              icon: Icon(Icons.close),
+            ),
+          ],
+          bottom: TabBar(
+            controller: _tcontorl,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.supervisor_account)),
+              Tab(icon: Icon(Icons.close)),
+            ],
+          ),
+        ),
+        body: //Center(child: Text(mtext)),
+        TabBarView(
+          controller: _tcontorl,
+          children: <Widget>[
+            Center(child: Text('Welcome to Home')),
+            Center(child: Text('Welcome to User account')),
+            Center(child: Text('Welcome to close page')),
+          ],
+        ),
+        bottomNavigationBar: Material(
+          color: Colors.blue,
+          child: TabBar(
+            controller: _tcontorl,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.arrow_back)),
+              Tab(icon: Icon(Icons.circle_outlined)),
+              Tab(icon: Icon(Icons.square_outlined)),
+            ],
+          ),
         ),
       ),
     );
