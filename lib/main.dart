76944d7eed8b8eb029da66febcce626d61a4f0f3
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-07-09 00:25:44 +0530

    Basic Textfield Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/customw.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,36 +14,34 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   String mtext = '';
 
-  late TabController _tcontorl;
-  @override
-  void initState() {
-    _tcontorl = TabController(length: 3, vsync: this);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Application',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          title: Text('Appbar'),
-          //centerTitle: true,
-          titleSpacing: 80.0,
-          elevation: 30.0,
-          //toolbarOpacity: 0.5,
-          leading: IconButton(
-            onPressed: () {
-              setState(() {
-                mtext = 'This is menu icon button';
-              });
-            },
-            icon: Icon(Icons.menu),
-          ),
+        appBar: AppBar(backgroundColor: Colors.grey, title: Text('Appbar')),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              /*onChanged: (String txt) {
+                setState(() {
+                  mtext = txt;
+                });
+              },*/
+              onSubmitted: (String txt) {
+                setState(() {
+                  mtext = txt;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Type here...',
+                labelText: 'Fullname',
+              ),
+            ),
+            Text(mtext),
+          ],
         ),
-        body: CustomWidgets(),
       ),
     );
   }
